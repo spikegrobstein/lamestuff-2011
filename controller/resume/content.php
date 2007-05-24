ee34355d4b8254164e99ea236068d2f3ef2fcb47
Spike Grobstein <spike666@mac.com>
2007-05-24 03:39:27 +0000

    added cert icons to resume page shrunk pdf icon to 64px added adobe cert info to education section renamed education section to "education/certification" updated computer skills to match Word file resume as a list updated job descriptions to match Word file resume updated accomplishments to match Word file resume added css for new icons

--- controller/resume/content.php
+++ controller/resume/content.php
@@ -13,6 +13,11 @@
 
 	</div>
 	
+	<div class="cert_icons">
+		<img src="/images/ACE_Illustrator.gif" alt="Adobe Certified Expert, Illustrator" />
+		<img src="/images/ACE_Photoshop.gif" alt="Adobe Certified Expert, Photoshop" />
+	</div>
+	
 	<h2>Objective:</h2>
 	<p>
 		Being self-motivated and having experience and extreme interest in art and technology, I hope to 
@@ -20,22 +25,39 @@
 		as possible while continuing my education in the related fields.
 	</p>
 
-	<h2>Education:</h2>
-	<p>
-		Rutgers University- New Brunswick, NJ (1999-2002)
-	</p>
+	<h2>Education/Certifications:</h2>
+	<ul>
+		<li>
+			Adobe Certified Expert, Illustrator CS2
+		</li>
+		<li>
+			Adobe Certified Expert, Photoshop CS2
+		</li>
+		<li>
+			Rutgers University- New Brunswick, NJ (1999-2002)
+		</li>
+	</ul>
 
 	<h2>Computer Skills:</h2>
-	<p>
-		Knowledge of computer and computer network architecture and security.<br/>
-		Understanding of secure application design concepts.<br/>
-		Use and administration of MacOS 7-9, MacOS X, Linux (Gentoo / Ubuntu / Kubuntu), BeOS, Windows, Solaris, NetBSD.<br/>
-		Guru-level of experience using Photoshop &amp; Illustrator<br/>
-		Experience with AfterEffects (including render-farms), Quark XPress, FlexiSIGN, InDesign, Microsoft Office (some Excel scripting),
-		XCode, Quickbooks, FilemakerPro<br/>
-		Technology experience: HTML/XHTML, XML, CSS, LDAP, MySQL, CVS, SVN, Firewalls (iptables).<br/>
-		Programming in Java, C, Objective-C (Cocoa), PERL, BASH, Applescript, VBA, PHP, Ruby / Ruby on Rails, Python.<br/>
-	</p>
+	<ul>
+		<li>
+			Knowledge of computer and computer network architecture and security. Understanding of secure
+			application design concepts.
+		</li>
+		<li>
+			Experienced with use and administration of MacOS 9/X, Linux (Gentoo/Ubuntu/CentOS), NetBSD,
+			OpenBSD, BeOS, Windows, Solaris.
+		</li>
+		<li>
+			Mastery of Photoshop, ImageReady, Illustrator, GoLive, AfterEffects, Premier, Quark XPress,
+			Freehand, InDesign, Microsoft Office (some Excel scripting), BBEdit, XCode, Quickbooks,
+			FilemakerPro, XHTML, CSS, LDAP, MySQL, CVS, Firewalls (iptables).
+		</li>
+		<li>
+			Programming skills include REALbasic, Java, C, Objective-C (Cocoa), PERL, BASH, Applescript,
+			VBA, PHP, Ruby / Ruby on Rails, Python.
+		</li>
+	</ul>
 
 	<h2>Experience:</h2>
 	<p>
@@ -43,15 +65,16 @@
 		<span class="company_description">Flags, Banners, Stage Backdrops, etc.</span>
 		<span class="position">Production Artist / IT Department</span>
 		<div class="job_description">
-			Prepared artwork for output on digital inkjet machines (on vinyl and fabric), screenprinting, appliqu&eacute;,
-			and embroidery. Redraw designs and logos. Operated plotter for appliqu&eacute; and screenprinting jobs. Computer
-			technical support for the PC and Mac network (Windows 2003 Fileserver, OSX and OS9 Macs,
-			Windows 98, 2000, and XP) at all 3 company locations. Wrote scripts for repetitive tasks in the Art Department
-			(file labeling, jpeg proofs, and emailing and CD burning for outsourced jobs) using Applescript and PERL. 
-			Built hardware and configured software for FTP server running Gentoo Linux including automatic notification
-			of file submissions. Wrote web application for client file submissions. Created and designed corporate website
-			using PHP, PERL, CSS and XHTML with seamless integration with FTP server's web front-end. Also other miscellaneous
-			application and script development and documentation.
+			Prepare artwork for output on digital inkjet machines (on vinyl and fabric), screenprinting,
+			appliqu&eacute;, and embroidery. Redraw designs and logos. Operate plotter for appliqu&eacute; and
+			screenprinting jobs. Perform computer tech support for the PC and Mac network (Windows 2003
+			Fileserver, OSX and OS9 Macs, Windows 98/2000/XP) at all three company locations. Write scripts
+			for repetitive tasks in the art department (file labeling, jpeg proofs, emailing, and CD burning
+			for outsourced jobs) using Applescript and PERL. Assemble hardware and configured software for
+			FTP server running Gentoo Linux, including automatic email notification of file submissions.
+			Write web application for client file submissions. Create and design corporate website using
+			PHP, PERL, CSS and XHTML, with seamless integration with FTP server’s web front-end. Also handle
+			miscellaneous application and script development and documentation.
 		</div>
 	</p>
 	<p>
@@ -59,10 +82,9 @@
 		<span class="company_description">Garment Screenprinting / Embroidery</span>
 		<span class="position">Co-owner / Art Director / IT Manager / Sales</span>
 		<div class="job_description">
-			Involvement in setting up of the new business including creating relationships with vendors,
-			purchasing computer hardware as well as production hardware and I was involved with billing, collections,
-			tax information and purchasing.  Quoted jobs,
-			worked with customers on generating custom artwork for screenprinting, wrote orders.
+			Involved in setting up the new business, including creating relationships with vendors
+			and purchasing computer and production hardware and software. Handled billing, collections,
+			tax information and sales. 
 		</div>
 	</p>
 	<p>
@@ -70,9 +92,8 @@
 		<span class="company_description">ISP Turnkey Solutions / ISP Branding</span>
 		<span class="position">Lead Graphic Artist</span>
 		<div class="job_description">
-			Designed batches of related graphics for branded websites and software following predetermined
-			size requirements and/or guidelines. Worked from home and emailed art making revisions as necessary.
-			Designs included web and print graphics.
+			Designed batches of related graphics for branded websites and software following
+			predetermined size requirements. Designs included web and print graphics.
 		</div>
 	</p>
 	<p>
@@ -80,10 +101,10 @@
 		<span class="company_description">On-Site Computer Technicians</span>
 		<span class="position">Computer Tech</span>
 		<div class="job_description">
-			Traveled to client offices to configure and/or analyze and repair computer problems, both hardware
-			and software. Had to work in environments with mixed platforms (OSX, Windows 98/2000/
-			XP and Windows 2003 Server). Wrote Applescript applications to automate connecting to shared network
-			drives. Put together Microsoft Word documentation for employees to access shared network drives.
+			Traveled to client offices to configure and / or analyze and repair computer problems, both
+			hardware and software. Worked in environments with mixed platforms (OSX, Windows 98/2000/XP,
+			and Windows 2003 Server). Wrote Applescripts to automate connecting to shared network drives.
+			Put together Microsoft Word documentation for employees to access shared network drives.
 		</div>
 	</p>
 	<p>
@@ -91,10 +112,10 @@
 		<span class="company_description">Direct ink jet printing on fabrics</span>
 		<span class="position">Production Artist</span>
 		<div class="job_description">
-			Set up artwork for the RIP, contacted customers about questions and problems, wrote software
-			to automate DVD-RAM backup system for OSX using Obj-C and PERL, operated Solaris based
-			RIP over Telnet, and operated printing machines. Also created their new web site using my 
-			HTML and a PERL template engine.
+			Set up artwork for the RIP, contacted customers regarding questions and problems, wrote software
+			to automate DVD-RAM backup system for OSX using Obj-C and PERL, operated Solaris-based
+			RIP over Telnet, and operated printing machines. Also wrote a hand-coded HTML and PERL
+			template engine to create a new company web site. 
 		</div>
 	</p>
 	<p>
@@ -103,9 +124,8 @@
 		<span class="company_description">Garment Screenprinting/ Embroidery</span>
 		<span class="position">Art Director</span>
 		<div class="job_description">
-			Scanning, artwork re-creation from customer hardcopy and original artwork, color separations
-			for screenprinting, invoicing, customer service, screenprinting and training of other employees,
-			contacted customers regarding questions about artwork and other aspects of orders.
+			Responsible for scanning, artwork re-creation from customer hardcopy and original
+			artwork, color separations for screenprinting, invoicing, customer service, and training employees
 		</div>
 	</p>
 	<p>
@@ -113,51 +133,100 @@
 		<span class="company_description">Digital Prepress</span>
 		<span class="position">Artist/ Scanning Technician</span>
 		<div class="job_description">
-			Artwork re-creation, photo retouching and manipulation, color correction, flatbed and drum
-			scanning of reflective art and transparencies.
+			Handled artwork re-creation, photo retouching and manipulation, color correction,
+			and flatbed and drum scanning of reflective art and transparencies.
 		</div>
 	</p>
 
 	<h2>Personal Projects &amp; Accomplishments:</h2>
 	<p>
-		Currently run a webserver (Gentoo Linux/PPC) with SSH, FTP, Apache, MySQL, PHP and Ruby on
-		Rails with over 20 users and websites. Set up personal file server (Gentoo Linux/PPC) on home LAN for
-		network access storage and automated daily snapshot backup of above webserver.
+		Currently run a webserver (Gentoo Linux/PPC) with SSH, FTP, Apache, MySQL, PHP, and
+		Ruby on Rails, with over 20 users and websites. Set up personal file server (Gentoo Linux/PPC)
+		on home LAN for network access storage and automated daily snapshot backup of webserver.
 	</p>
 	<p>
-		Created a full-functioning invoicing system tailored for freelance jobs and scripts for emailing PDFs
-		of invoices and quotes in Filemaker Pro.
+		Created a fully-functional invoicing system tailored for freelance jobs with scripts for
+		emailing PDFs of invoices and quotes in Filemaker Pro.
 	</p>
 	<p>
-		Contract work at a private school: network troubleshooting on a medium sized network (200 nodes). User account
-		migration between 2 OSX Server machines. Disaster recovery. Automated a file restore/recovery procedure on x86 Linux
-		(Ubunutu) to new PPC server. Layer 1: Terminating cables, punchdown, crimping. Machine reloads. Some exposure to
-		Sonicwall content filter. Automated installs for lab machines. Automated home folder persistance on lab machines.
+		Contract work at a private school:
+	</p>
+	<ul>
+		<li>
+			Troubleshooting on a medium-sized network (over 200 nodes).
+		</li>
+		<li>
+			User account migration between two OSX Server machines.
+		</li>
+		<li>
+			Disaster recovery.
+		</li>
+		<li>
+			Automated a file restore / recovery procedure on x86 Linux (Ubuntu) to new PPC server.
+		</li>
+		<li>
+			Layer 1: Terminating cables, punchdown, crimping.
+		</li>
+		<li>
+			Machine reloads.
+		</li>
+		<li>
+			Some exposure to Sonicwall content filter.
+		</li>
+		<li>
+			Automated installs for lab machines.
+		</li>
+		<li>
+			Automated home folder persistence on lab machines.
+		</li>
+	</ul>
+	<p>
+		Collaboration on web-based firewall frontend using PHP and BASH scripting. Designed and
+		implemented module framework, then integrated into existing code. Created error handling
+		and reporting framework for project.
 	</p>
 	<p>
-		Collaboration on web-based firewall frontend using PHP and BASH scripting. Designed and implemented module
-		framework, then integrated into existing code. Created error handling and reporting framework for project.
+		Miscellaneous contract work configuring machines in mixed environments:
 	</p>
+	<ul>
+		<li>
+			Set up OSX Server to host Bugzilla for company’s internal bug reporting and handling framework.
+		</li>
+		<li>
+			Configured centralized LDAP authentication server in Gentoo Linux.
+		</li>
+		<li>
+			Windows 2003 Server filesharing to OSX machines.
+		</li>
+		<li>
+			OSX printing to Windows 2003 Server.
+		</li>
+		<li>
+			Automated OSX backup to Windows 2003 Server.
+		</li>
+		<li>
+			Customized software for auto-mounting server shares in OSX using Applescript.
+		</li>
+		<li>
+			Hard drive and RAM installation on production machines.
+		</li>
+		<li>
+			High-end printer configuration in OSX (non-standard drivers, Linux/Unix drivers in OSX).
+		</li>
+		<li>
+			Hewlett Packard printer configuration.
+		</li>
+	</ul>
 	<p>
-		Miscellaneous contract work configuring machines: Set up OSX Server to host Bugzilla for company's internal
-		bug reporting and handling framework. Configured centralized LDAP authentication server in Gentoo Linux.
-		Mixed environment integration: Windows 2003 Server filesharing to OSX machines. OSX printing to Windows 2003
-		Server. Automated OSX backup to Windows 2003 Server. Customized software for mounting server shares in OSX using
-		Applescript. Hard drive and RAM installation on production machines.
-		High-end printer configuration in OSX (non-standard drivers, Linux/Unix drivers in OSX). Hewlett Packard printer
-		configuration.
-	</p>
-	<p>
-		Wrote an open source <a href="http://gcmtool.sourceforge.net">program</a> for manipulating Nintendo
-		Gamecube disc images (.gcm and .iso) for Linux, OSX and Windows (under cygwin) that runs on both
-		PPC and x86 architectures in ANSI C. Also wrote an open source
-		<a href="http://nesromtool.sourceforge.net">program</a> for extracting and injecting graphics as well as
+		Wrote an open source program for manipulating Nintendo GameCube disc images (.gcm and .iso)
+		for Linux, OSX, and Windows (under cygwin) that runs on both PPC and x86 architectures in
+		ANSI C. Also wrote an open source program for extracting and injecting graphics as well as
 		editing meta-data (titles) for Nintendo (NES) game ROM files. Written in ANSI C for *nix and OSX.
 	</p>
 	<p>
-		Created several applications in Java using TCP and UDP socket communication utilizing the Hotline
-		Connect and Hotline Tracker protocols including a Hotline Connect client and Hotline Tracker server
-		and client.
+		Created several applications in Java using TCP and UDP socket communication utilizing the
+		Hotline Connect and Hotline Tracker protocols including a Hotline Connect client and Hotline
+		Tracker server and client.
 	</p>
 	<p>
 		Developed the only working Mac client for the ScourXChange network using REALbasic.
