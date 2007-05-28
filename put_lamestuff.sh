de661dd077358932dafa86796e1053a5ad7400e7
Spike Grobstein <spike666@mac.com>
2007-05-28 19:56:14 +0000

    modified put script to place files in test/ instead of test/public/

--- put_lamestuff.sh
+++ put_lamestuff.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [[ "$1" = 'test' ]]; then
-	rsync -azv --exclude counter.dat * --delete lamestuff@lamestuff.com:test/public/
+	rsync -azv --exclude counter.dat * --delete lamestuff@lamestuff.com:test/
 else
 	rsync -azv --exclude counter.dat * --delete lamestuff@lamestuff.com:public_html/
 fi
