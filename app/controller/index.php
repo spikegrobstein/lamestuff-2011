b683b312919d6a445f50e3a5e92bf7377624eb24
Spike Grobstein <spike666@mac.com>
2007-07-23 15:10:20 +0000

    updated description to include info about current work.

--- app/controller/index.php
+++ app/controller/index.php
@@ -1,9 +1,8 @@
 <p>
 	Hi, my name is Spike Grobstein. I'm a freelance artist, programmer,
 	technophile and all-around geek. Currently living in Brooklyn, NY, I work as
-	a production artist at flag &amp; banner company and also handle brokering and
-	artwork of screen printing orders as well as managing several open source projects
-	on the side.
+	a systems administrator at a large record company in the development department
+	as well as manage several open source projects on the side.
 </p>
 
 <div class="btn_container">
@@ -17,7 +16,7 @@
 
 <p>
 	I've got 10 years experience in offset printing and digital prepress and 6 years
-	experience in screen printing and embroidery. I've been a hobbiest programmer for
+	experience in screen printing and embroidery. I've been a hobbyist programmer for
 	14 years and have dabbled in most popular languages (PERL, C, ObjC, BASIC, PHP, Python,
 	Ruby, bash, AppleScript). I'm also an operating system slut and have been known to use
 	everything from OSX to Linux to Windows to BeOS and everything in between.
@@ -26,7 +25,7 @@
 <p>
 	I'm currently available for freelance work; be it design work or software
 	development. I'm highly proficient in PERL, C and AppleScript, on the programming end,
-	and consider myself to be a full-fledged guru in Adobe Illustrator and Photoshop.
+	and am a full-fledged guru (aka Certified Expert) in Adobe Illustrator and Photoshop.
 	<a href="/portfolio/" title="Examples of Previous Work">Examples</a> of my work are available as well as a complete
 	<a href="/resume/" title="Work History and Skillset">r&eacute;sum&eacute;</a>. At the moment,
 	I'm specializing in design of flyers and automation of repetitive tasks in OSX using AppleScript.
